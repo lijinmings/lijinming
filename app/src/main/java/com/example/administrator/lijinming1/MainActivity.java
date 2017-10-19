e062bcfeecd6fae7f2ebfb0a53335884076599a5
lijinmings <805156197@qq.com>
2017-10-19 11:09:40 +0800

    tiaoz

--- app/src/main/java/com/example/administrator/lijinming1/MainActivity.java
+++ app/src/main/java/com/example/administrator/lijinming1/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.administrator.lijinming1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button lijinming1=(Button) findViewById(R.id.lijinming1);
+        lijinming1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent=new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
