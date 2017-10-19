011958c7345536ab34f54b6a05bc065524512db6
lijinmings <805156197@qq.com>
2017-10-19 11:27:49 +0800

    haha

--- app/src/main/java/com/example/administrator/lijinming1/LoginActivity.java
+++ app/src/main/java/com/example/administrator/lijinming1/LoginActivity.java
@@ -1,9 +1,12 @@
 package com.example.administrator.lijinming1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Administrator on 2017/10/16.
@@ -14,6 +17,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button ben=(Button) findViewById(R.id.ben);
+        ben.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent=new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
